7fa29de1fd6ca39675d922adb2b0343a328eb9c4
Adrian A. Dragulescu <adrian.dragulescu@gmail.com>
2014-08-21 00:00:00 +0000

    version 0.6.1

--- R/utilities.R
+++ R/utilities.R
@@ -1,12 +1,12 @@
-.onLoad <- function(libname, pkgname)
-{
-  .jpackage(pkgname)
+## .onLoad <- function(libname, pkgname)
+## {
+##   .jpackage(pkgname)
   
-  # what's your java  version?  Need >= 1.5.0.
-  jversion <- .jcall('java.lang.System','S','getProperty','java.version')
-  if (jversion < "1.5.0")
-    stop(paste("Your java version is ", jversion,
-                 ".  Need 1.5.0 or higher.", sep=""))
+##   # what's your java  version?  Need >= 1.5.0.
+##   jversion <- .jcall('java.lang.System','S','getProperty','java.version')
+##   if (jversion < "1.5.0")
+##     stop(paste("Your java version is ", jversion,
+##                  ".  Need 1.5.0 or higher.", sep=""))
 
-}
+## }
 
